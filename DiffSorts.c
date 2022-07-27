39d577e14def24b4143d9874dc25278845eb774f
Rishikant <rishiruthika@gmail.com>
2022-07-27 19:14:40 +0530

    MERGE SORT ADDED

--- DiffSorts.c
+++ DiffSorts.c
@@ -5,6 +5,9 @@ void swap(int *x, int *y);
 void insertion_sort(int a[], int s);
 void selection_sort(int a[], int s);
 void bubble_sort(int a[], int s);
+void printArray(int A[], int size);
+void merge(int a[], int l, int m, int r);
+void merge_sort(int a[], int l, int s);
 
 int main()
 {
@@ -45,6 +48,13 @@ int main()
         case 3: 
         {
             bubble_sort(a, n);
+            break;
+        }
+
+        case 4: 
+        {
+            merge_sort(a, 0, n-1);
+            printArray(a, n);
             break;
         }
     }
@@ -129,5 +139,71 @@ void bubble_sort(int a[], int n)
     for(i=0;i<n;i++)
     {
         printf("%d ", a[i]);
+    }
+}
+
+void printArray(int A[], int size)
+{
+    int i;
+    for (i = 0; i < size; i++)
+        printf("%d ", A[i]);
+    printf("\n");
+}
+
+void merge(int arr[], int l, int m, int r) //function that  merges the two array 
+{
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+ 
+    /* create temp arrays */
+    int L[n1], R[n2];
+ 
+    /* Copy data to temp arrays L[] and R[] */
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+ 
+    /* Merge the temp arrays back into arr[l..r]*/
+    i = 0; // Initial index of first subarray
+    j = 0; // Initial index of second subarray
+    k = l; // Initial index of merged subarray
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        }
+        else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+ 
+    /* Copy the remaining elements of L[], if there
+    are any */
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+ 
+    /* Copy the remaining elements of R[], if there
+    are any */
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+void merge_sort(int arr[], int l, int r) // function that uses recursion to implement merge sort 
+{
+    if (l < r) {
+        int m = l + (r - l) / 2;
+        // Sort first and second halves
+        merge_sort(arr, l, m);
+        merge_sort(arr, m + 1, r);
+        merge(arr, l, m, r);
     }
 }
