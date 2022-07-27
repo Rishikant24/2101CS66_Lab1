3180360d84ba705d3971ca0e93b6155f4884ce32
Rishikant <rishiruthika@gmail.com>
2022-07-27 11:46:23 +0530

    User Input Part

---
+++ Calculator Program.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{   
+    float a, b, ans;
+    int n;
+    
+    //Input 2 numbers
+    printf("Enter two numbers on which you want to perform arithmetic operation: \n");
+    scanf("%f%f",&a, &b);
+    
+    // Select operation
+    printf("Choose the Operation you want to perform:\n");
+    printf("Enter 1 for Addition\n");
+    printf("Enter 2 for Subtraction\n");
+    printf("Enter 3 for Multiplication\n");
+    printf("Enter 4 for Division\n");
+    printf("Enter 5 for Modulus Function\n");
+    scanf("%d", &n);
+
+}
